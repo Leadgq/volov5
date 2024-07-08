41d93703f76f169895f0ec8b5ca0c297a2b0f677
gaoqiang <2643336540@qq.com>
2024-07-08 16:29:31 +0800

    volov5

--- train.py
+++ train.py
@@ -517,14 +517,18 @@ def train(hyp, opt, device, callbacks):
 def parse_opt(known=False):
     """Parses command-line arguments for YOLOv5 training, validation, and testing."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weights", type=str, default=ROOT / "yolov5s.pt", help="initial weights path")
-    parser.add_argument("--cfg", type=str, default="", help="model.yaml path")
-    parser.add_argument("--data", type=str, default=ROOT / "data/coco128.yaml", help="dataset.yaml path")
+    #初始权重
+    parser.add_argument('--weights', type=str, default='yolov5s.pt', help='initial weights path')#修改处
+    # 训练的模型文件
+    parser.add_argument('--cfg', type=str, default='my_dates/mydata_1.yaml', help='model.yaml path')  # 修改处
+    # 数据参数文件
+    parser.add_argument('--data', type=str, default='my_dates/mydata.yaml', help='dataset.yaml path')  # 修改处
     parser.add_argument("--hyp", type=str, default=ROOT / "data/hyps/hyp.scratch-low.yaml", help="hyperparameters path")
-    parser.add_argument("--epochs", type=int, default=100, help="total training epochs")
+    # 修改处，显卡低的可以调到100，原来是300
+    parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument("--batch-size", type=int, default=16, help="total batch size for all GPUs, -1 for autobatch")
     parser.add_argument("--imgsz", "--img", "--img-size", type=int, default=640, help="train, val image size (pixels)")
-    parser.add_argument("--rect", action="store_true", help="rectangular training")
+    parser.add_argument('--rect', action='store_true', help='rectangular training')
     parser.add_argument("--resume", nargs="?", const=True, default=False, help="resume most recent training")
     parser.add_argument("--nosave", action="store_true", help="only save final checkpoint")
     parser.add_argument("--noval", action="store_true", help="only validate final epoch")
@@ -543,7 +547,7 @@ def parse_opt(known=False):
     parser.add_argument("--single-cls", action="store_true", help="train multi-class data as single-class")
     parser.add_argument("--optimizer", type=str, choices=["SGD", "Adam", "AdamW"], default="SGD", help="optimizer")
     parser.add_argument("--sync-bn", action="store_true", help="use SyncBatchNorm, only available in DDP mode")
-    parser.add_argument("--workers", type=int, default=8, help="max dataloader workers (per RANK in DDP mode)")
+    parser.add_argument('--workers', type=int, default=0, help='maximum number of dataloader workers')
     parser.add_argument("--project", default=ROOT / "runs/train", help="save to project/name")
     parser.add_argument("--name", default="exp", help="save to project/name")
     parser.add_argument("--exist-ok", action="store_true", help="existing project/name ok, do not increment")
